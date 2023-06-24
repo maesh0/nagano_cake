bb123b791d8ae1027f82e0fb76c0e33c16cf4c3e
EC2 Default User <ec2-user@ip-172-31-40-200.ap-northeast-1.compute.internal>
2023-06-24 13:23:25 +0000

    [Add]ホーム画面、アバウト画面作成前

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# 顧客用
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+root to: 'public/homes#top'
+get 'about' => 'public/homes#about'
+
+
+namespace :customers do
+
+end
+
+namespace :admin do
+
+end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
