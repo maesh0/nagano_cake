bfdc784ad91448172a560708973d45ab47304a67
EC2 Default User <ec2-user@ip-172-31-40-200.ap-northeast-1.compute.internal>
2023-07-01 11:39:35 +0000

    [Add]ログイン後、登録後の遷移先

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,7 @@
+class Public::CustomersController < ApplicationController
+before_action :authenticate_customer!
+  def show
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,20 @@ root to: 'public/homes#top'
 
 get "/about" => "public/homes#about"
 
+get "/items" => "public/items#index"
+
+get "/items/:id" => "public/items#show"
+
+get "customers/my_page" => "public/customers#show"
+
 
 namespace :customers do
 
 end
 
 namespace :admin do
+
+  root to: 'homes#top'
 
 end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
